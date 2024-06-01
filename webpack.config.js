7cf101673564483d3ea958e41780b338bb3450d5
Amir Ali <amiraliazimloo123@gmail.com>
2024-06-01 00:21:19 -0700

    Add webpack and babel config.

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    modoule: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
+}
