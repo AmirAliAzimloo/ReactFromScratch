e691ab2e93ddfbc3b8017041337cff651eecc2b2
Amir Ali <amiraliazimloo123@gmail.com>
2024-06-01 01:17:37 -0700

    Add the React config.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('Hello World!');
+import ReactDOM from 'react-dom/client';
+
+function App() {
+    return (
+        <div>
+            Hello World!
+        </div>
+    );
+}
+
+const container = document.createElement('div');
+const root = ReactDOM.createRoot(container);
+root.render(<App/>);
