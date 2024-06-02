c23287fbc7b5b09fd3c7e60c60c9ed92c54528ba
Amir Ali <amiraliazimloo123@gmail.com>
2024-06-01 21:16:17 -0700

    Add the tailwind config.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const App: React.FC = () => {
     return (
-        <div style={{background: "green"}}>
+        <div className="bg-blue-500 text-white p-4">
             Hello World!
         </div>
     );
